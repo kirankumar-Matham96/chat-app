f64b28b474f23c44126d51b4abb4a989925ea211
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-08-21 12:00:31 +0530

    feat: added chat page header

--- src/components/RightContainer/index.jsx
+++ src/components/RightContainer/index.jsx
@@ -6,7 +6,23 @@ export const RightContainer = () => {
   return (
     <div className={rightContainerStyles.bgContainer}>
       <div className={rightContainerStyles.mainContainer}>
-        Main message container
+        <div className={rightContainerStyles.mainChatHeader}>
+          <div className={rightContainerStyles.headerImage}>
+            <img
+              src="https://res.cloudinary.com/do4v7miwh/image/upload/v1642074083/samples/animals/three-dogs.jpg"
+              alt="1group icon"
+            />
+          </div>
+          <div className={rightContainerStyles.headerTextContainer}>
+            <p className={rightContainerStyles.groupName}>Group name</p>
+            <p className={rightContainerStyles.groupMembersList}>
+              Group members names, person 1, person 2, person 1, person 2,
+              person 1, person 2, person 1, person 2, person 1, person 2, person
+              1, person 2, person 1, person 2, person 1, person 2, person 1,
+              person 2, person 1, person 2
+            </p>
+          </div>
+        </div>
       </div>
       <div className={rightContainerStyles.inputContainer}>
         <MessageInput />
